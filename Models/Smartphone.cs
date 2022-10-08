b0ed7bc2f1fbd3f90fbd48f2b4eeba7f32993110
Márcio <marciaohbs@gmail.com>
2022-10-08 14:25:50 -0300

    Desafio POO Concluido

--- Models/Smartphone.cs
+++ Models/Smartphone.cs
@@ -2,14 +2,71 @@ namespace DesafioPOO.Models
 {
     public abstract class Smartphone
     {
-        public string Numero { get; set; }
-        // TODO: Implementar as propriedades faltantes de acordo com o diagrama
-
-        public Smartphone(string numero)
+        public Smartphone(string numero, string modelo, string imei, int memoria)
         {
             Numero = numero;
-            // TODO: Passar os parâmetros do construtor para as propriedades
+            Modelo = modelo;
+            Memoria = memoria;
+            IMEI = imei;
         }
+
+        private string modelo;
+        private string numero;
+        private string imei;
+        private int memoria;
+        public string Modelo 
+        {
+             get => modelo; 
+             private set
+             {
+                if(Modelo==null)
+                {
+                    modelo = value;
+                }
+                else
+                {
+                    throw new Exception("O Telefone já possui um modelo e não pode ser alterado");
+                }
+             }
+        }
+        public string IMEI 
+        {
+             get => imei; 
+             private set
+             {
+                if(IMEI==null)
+                {
+                    imei = value;
+                }
+                else
+                {
+                    throw new Exception("O Telefone já possui um IMEI e não pode ser alterado");
+                }
+             }
+        }
+        
+        public int Memoria
+        {
+             get => memoria; 
+             private set
+             {
+                if(Memoria==0)
+                {
+                    memoria = value;
+                }
+                else
+                {
+                    throw new Exception("O Telefone já possui Memoria e não pode ser alterado");
+                }
+             }
+        }
+        public string Numero 
+        { 
+            get => numero; 
+
+            set => numero = value;
+        }
+
 
         public void Ligar()
         {
--- Program.cs
+++ Program.cs
@@ -1,3 +1,29 @@
 ﻿using DesafioPOO.Models;
 
-// TODO: Realizar os testes com as classes Nokia e Iphone
+Iphone iphone1 = new Iphone("12316584","Iphone3","8887778",256);
+Nokia nokia1 = new Nokia("98765433","Nokia1","1117778",128);
+
+
+Console.WriteLine($"Telefone:\r\nNúmero: {iphone1.Numero}\r\nModelo: {iphone1.Modelo}\r\nIMEI:{iphone1.IMEI}\r\nMemória:{iphone1.Memoria}");
+iphone1.Ligar();
+iphone1.ReceberLigacao();
+iphone1.InstalarAplicativo("APP Iphone");
+iphone1.Numero = "1234568";
+Console.ReadLine();
+Console.Clear();
+
+Console.WriteLine($"Telefone:\r\nNúmero: {iphone1.Numero}\r\nModelo: {iphone1.Modelo}\r\nIMEI:{iphone1.IMEI}\r\nMemória:{iphone1.Memoria}");
+Console.ReadLine();
+Console.Clear();
+
+Console.WriteLine($"Telefone:\r\nNúmero: {nokia1.Numero}\r\nModelo: {nokia1.Modelo}\r\nIMEI:{nokia1.IMEI}\r\nMemória:{nokia1.Memoria}");
+nokia1.Ligar();
+nokia1.ReceberLigacao();
+nokia1.InstalarAplicativo("APP Nokia");
+Console.ReadLine();
+Console.Clear();
+
+nokia1.Numero = "55544446";
+Console.WriteLine($"Telefone:\r\nNúmero: {nokia1.Numero}\r\nModelo: {nokia1.Modelo}\r\nIMEI:{nokia1.IMEI}\r\nMemória:{nokia1.Memoria}");
+Console.ReadLine();
+Console.Clear();
